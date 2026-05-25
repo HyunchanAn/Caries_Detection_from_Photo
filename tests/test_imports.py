47dce35b89a78cf8ae3375ea3423ffb2eae9a975
Hyunchan An <chemahc94@gmail.com>
2026-05-25 21:47:35 +0900

    fix: Ignore unused import warnings in test_imports.py

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -11,12 +11,12 @@ def test_imports():
         import ultralytics
 
         # 패키지 내부 모듈 테스트
-        from alphadent import app
-        from alphadent import benchmark
-        from alphadent import inference
-        from alphadent import train
-        from alphadent import valid
-        import streamlit_app
+        from alphadent import app  # noqa: F401
+        from alphadent import benchmark  # noqa: F401
+        from alphadent import inference  # noqa: F401
+        from alphadent import train  # noqa: F401
+        from alphadent import valid  # noqa: F401
+        import streamlit_app  # noqa: F401
 
         imports_successful = True
     except ImportError as e:
